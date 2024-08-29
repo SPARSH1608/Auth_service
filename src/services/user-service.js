0d147b45d782b5b1dd679ed3ba73507cb3b71fd3
Sparsh <sparshgoelk@gmail.com>
2024-08-29 18:01:09 +0530

    added sigin functionality

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 const UserRepository = require('../repository/user-repository');
 const jwt = require('jsonwebtoken');
 const { JWT_KEY } = require('../config/serverConfig');
+const bcrypt = require('bcrypt');
 class UserService {
   constructor() {
     this.userRepository = new UserRepository();
@@ -33,6 +34,33 @@ class UserService {
       return response;
     } catch (error) {
       console.log('Something went wrong in token validation', error);
+      throw error;
+    }
+  }
+
+  checkPassword(userInputPlainPassword, encryptedPassword) {
+    try {
+      return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+    } catch (error) {
+      console.log('Something went wrong in password comparison', error);
+      throw error;
+    }
+  }
+
+  async signIn(email, plainPassword) {
+    try {
+      const user = await this.userRepository.getByEmail(email);
+      if (user) {
+        const passwordsMatch = this.checkPassword(plainPassword, user.password);
+        if (!passwordsMatch) {
+          console.log('Password mismatch');
+          throw { error: 'Incorrect Password' };
+        }
+        const newJWT = this.createToken({ email: user.email, id: user.id });
+        return newJWT;
+      }
+    } catch (error) {
+      console.log('Something went wrong while sign in process', error);
       throw error;
     }
   }
