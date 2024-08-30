5ceef73cccdf706aa9be146a685f7c9a53a18bae
Sparsh <sparshgoelk@gmail.com>
2024-08-30 13:41:53 +0530

    added roles models and user roles table

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const db = require('./models/index');
+const { User, Role } = require('./models/index');
+
 const apiRoutes = require('./routes/index');
 const { PORT } = require('./config/serverConfig');
 
@@ -9,8 +12,20 @@ const prepareAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use('/api', apiRoutes);
-  app.listen(PORT, () => {
+
+  app.listen(PORT, async () => {
     console.log(`Server started on port ${PORT}`);
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
+
+    // const u1 = await User.findByPk(4);
+    // const r1 = await Role.findByPk(1);
+    // // u1.addRole(r1);
+    // const response = await u1.getRoles();
+    // console.log(response);
+    // const result = await u1.hasRole(r1);
+    // console.log(result);
   });
 };
 prepareAndStartServer();
